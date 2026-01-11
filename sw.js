04876dd7ab9a5a00cdf6bd9252281f7e99c3cb70
manaarsheikh0-ai <manaarsheikh0@gmail.com>
2026-01-12 04:24:20 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,30 @@
-self.addEventListener("install", e => {
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener("activate", () => {
+  self.clients.claim();
 });
 
-self.addEventListener("notificationclick", event => {
+self.addEventListener("notificationclick", function (event) {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow("./")
+    clients.openWindow("/")
   );
+});
+
+self.addEventListener("message", function (event) {
+  if (event.data.type === "schedule") {
+    const { title, body, time } = event.data;
+
+    const delay = time - Date.now();
+    if (delay > 0) {
+      setTimeout(() => {
+        self.registration.showNotification(title, {
+          body,
+          icon: "/manifest.json",
+        });
+      }, delay);
+    }
+  }
 });
